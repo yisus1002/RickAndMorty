4d66aa71cafd7403d218ec0a265d1ce433bf2c99
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-23 15:39:51 -0500

    Se creo el Home de la aplicación

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+
+  }
+  navegar(name: string){
+    this.router.navigate([`/${name}`])
+  }
+}
