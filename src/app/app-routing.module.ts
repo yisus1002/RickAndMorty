3b73143b666cc487233d36066783b8718cd60f3d
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-23 15:48:35 -0500

    Se crearon rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'characters', component: CharactersComponent},
+  {path: 'episodes', component: EpisodesComponent},
+  {path: "", redirectTo: "home", pathMatch: "full",},
+  {path: '**', redirectTo: "home", pathMatch: "full",},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
+import { CharacterComponent } from './shared/character/character.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterComponent } from './shared/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CharactersComponent,
+    EpisodesComponent,
+    CharacterComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
