86c04f8076230ff66aa9165a4e5fd57b40618429
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-25 18:23:11 -0500

    Se inserto el componente character al home en favorites

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,33 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+// import { parse } from 'graphql';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor(private router:Router) { }
-
+export class HomeComponent implements OnInit,  DoCheck {
+  favoriteCharacter:any;
+  constructor(private router:Router) { 
+    
+  }
+  ngDoCheck(): void { 
+    this.getData()
+  }
+  
   ngOnInit(): void {
+    this.getData()
 
   }
   navegar(name: string){
     this.router.navigate([`/${name}`])
+  }
+
+  getData(){
+    const data =(localStorage.getItem('favoriteCharacter'));
+
+    this.favoriteCharacter=(JSON.parse(data!));
+    // console.log(this.favoriteCharacter)
   }
 }
