4f4892b512c7836ce487292e7b4f3737729bfac1
yisus1002 <yisusgarcia1002@gmail.com>
2022-10-25 18:17:40 -0500

    Se creo un servicio para consumir Graphql y la api de charactersh

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,9 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+import {Observable, map,} from "rxjs"
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
+  api:string='https://rickandmortyapi.com/api/character/';
+  constructor(private apollo: Apollo,
+    public http: HttpClient) { 
+  }
+  
+  list(): Observable<any> {
+    return this.apollo.query({
+      query: gql`query {
+        characters{
+          info {
+            count,
+            pages,
+            next
+          }
+          results {
+            id,
+            name,
+            status,
+            species,
+            type,
+            gender,
+            image
+          }
+        }
+      }`
+    }).pipe(map((data:any)=>data['data']))
+  }
+  public getCharacter(name:string, status:string,type:string, gender:string, page:number){
+    const url =`${this.api}?name=${name}&status=${status}&type=${type}&gender=${gender}&page=${page}`
+  return  this.http.get(url).pipe(map((data:any)=>data))
+  }
 
-  constructor() { }
+  public getCharacterId(id:number){
+    const url =`${this.api}${id}`
+    return this.http.get(url) 
+  }
 }
